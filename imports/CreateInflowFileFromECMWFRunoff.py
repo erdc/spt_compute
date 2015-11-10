e22bbd385b7995cb9516ffcfdfc8e8ee7d7ca07a
snowman2 <alan.d.snow@byu.edu>
2015-11-10 09:15:57 -0600

    Modified code to run ecmwf low res at full temporal resolution

--- imports/CreateInflowFileFromECMWFRunoff.py
+++ imports/CreateInflowFileFromECMWFRunoff.py
@@ -31,8 +31,10 @@ class CreateInflowFileFromECMWFRunoff(object):
         self.header_wt = ['StreamID', 'area_sqm', 'lon_index', 'lat_index', 'npoints', 'weight', 'Lon', 'Lat']
         self.dims_oi = ['lon', 'lat', 'time']
         self.vars_oi = ["lon", "lat", "time", "RO"]
-        self.length_time = {"LowRes": 61, "HighRes": 125}
-        self.length_time_opt = {"LowRes": 60, "HighRes-1hr": 90, "HighRes-3hr": 48, "HighRes-6hr": 40, 
+        self.length_time = {"LowRes": 61, "LowResFull": 85,"HighRes": 125}
+        self.length_time_opt = {"LowRes-6hr": 60,
+                                "LowResFull-3hr-Sub": 48, "LowResFull-6hr-Sub": 36,
+                                "HighRes-1hr": 90, "HighRes-3hr": 48, "HighRes-6hr": 40, 
                                 "HighRes-3hr-Sub": 18, "HighRes-6hr-Sub": 16}
         self.errorMessages = ["Missing Variable 'time'",
                               "Incorrect dimensions in the input ECMWF runoff file.",
@@ -47,12 +49,12 @@ class CreateInflowFileFromECMWFRunoff(object):
         """Check the necessary dimensions and variables in the input netcdf data"""
         data_nc = NET.Dataset(in_nc)
 
-        dims = data_nc.dimensions.keys()
-        if dims != self.dims_oi:
+        nc_dims = data_nc.dimensions.keys()
+        if nc_dims != self.dims_oi:
             raise Exception(self.errorMessages[1])
 
-        vars = data_nc.variables.keys()
-        if vars != self.vars_oi:
+        nc_vars = data_nc.variables.keys()
+        if nc_vars != self.vars_oi:
             raise Exception(self.errorMessages[2])
 
         return
@@ -66,10 +68,13 @@ class CreateInflowFileFromECMWFRunoff(object):
         diff = NUM.unique(NUM.diff(time))
         data_nc.close()
         time_interval_highres = NUM.array([1.0,3.0,6.0],dtype=float)
+        time_interval_lowres_full = NUM.array([3.0, 6.0],dtype=float)
         time_interval_lowres = NUM.array([6.0],dtype=float)
-        if (diff == time_interval_highres).all():
+        if NUM.array_equal(diff, time_interval_highres):
             return "HighRes"
-        elif (diff == time_interval_lowres).all():
+        elif NUM.array_equal(diff, time_interval_lowres_full):
+            return "LowResFull"
+        elif NUM.array_equal(diff, time_interval_lowres):
             return "LowRes"
         else:
             return None
@@ -123,8 +128,15 @@ class CreateInflowFileFromECMWFRunoff(object):
 
         # Obtain size information
         if id_data == "LowRes":
-            size_time = self.length_time_opt["LowRes"]
-        else:
+            size_time = self.length_time_opt["LowRes-6hr"]
+        elif id_data == "LowResFull":
+            if in_time_interval == "3hr_subset":
+                size_time = self.length_time_opt["LowResFull-3hr-Sub"]
+            elif in_time_interval == "6hr_subset":
+                size_time = self.length_time_opt["LowResFull-6hr-Sub"]
+            else:
+                size_time = self.length_time_opt["LowRes-6hr"]
+        else: #HighRes
             if in_time_interval == "1hr":
                 size_time = self.length_time_opt["HighRes-1hr"]
             elif in_time_interval == "3hr":
@@ -194,6 +206,23 @@ class CreateInflowFileFromECMWFRunoff(object):
                 #ro_stream = data_goal * area_sqm_npoints
                 ro_stream = NUM.subtract(data_goal[1:,],data_goal[:-1,]) * area_sqm_npoints
 
+            #For data with the full version of Low Resolution, from Hour 0 to 144 (the first 49 time points) are of 3 hr time interval,
+            # then from Hour 144 to 360 (36 time points) are of 6 hour time interval
+            elif id_data == "LowResFull":
+                if in_time_interval == "3hr_subset":
+                    #use only the 3hr time interval
+                    ro_stream = NUM.subtract(data_goal[1:49,], data_goal[:48,]) * area_sqm_npoints
+                elif in_time_interval == "6hr_subset":
+                    #use only the 6hr time interval
+                    ro_stream = NUM.subtract(data_goal[49:,], data_goal[48:-1,]) * area_sqm_npoints
+                else: #"LowRes-6hr"
+                    #convert all to 6hr
+                    # calculate time series of 6 hr data from 3 hr data
+                    ro_6hr_a = NUM.subtract(data_goal[2:49:2,], data_goal[:48:2,])
+                    # get the time series of 6 hr data
+                    ro_6hr_b = NUM.subtract(data_goal[49:,], data_goal[48:-1,])
+                    # concatenate all time series
+                    ro_stream = NUM.concatenate([ro_6hr_a, ro_6hr_b]) * area_sqm_npoints
             #For data with High Resolution, from Hour 0 to 90 (the first 91 time points) are of 1 hr time interval,
             # then from Hour 90 to 144 (18 time points) are of 3 hour time interval, and from Hour 144 to 240 (16 time points)
             # are of 6 hour time interval
@@ -212,7 +241,7 @@ class CreateInflowFileFromECMWFRunoff(object):
                     ro_stream = NUM.subtract(data_goal[91:109,], data_goal[90:108,]) * area_sqm_npoints
                 elif in_time_interval == "6hr_subset":
                     #use only the 6hr time interval
-                    ro_stream = NUM.subtract(data_goal[109:,], data_goal[108:124,]) * area_sqm_npoints
+                    ro_stream = NUM.subtract(data_goal[109:,], data_goal[108:-1,]) * area_sqm_npoints
                 else: # in_time_interval == "6hr"
                     #arcpy.AddMessage("6hr")
                     # calculate time series of 6 hr data from 1 hr data
@@ -220,7 +249,7 @@ class CreateInflowFileFromECMWFRunoff(object):
                     # calculate time series of 6 hr data from 3 hr data
                     ro_6hr_b = NUM.subtract(data_goal[92:109:2,], data_goal[90:107:2,])
                     # get the time series of 6 hr data
-                    ro_6hr_c = NUM.subtract(data_goal[109:,], data_goal[108:124,])
+                    ro_6hr_c = NUM.subtract(data_goal[109:,], data_goal[108:-1,])
                     # concatenate all time series
                     ro_stream = NUM.concatenate([ro_6hr_a, ro_6hr_b, ro_6hr_c]) * area_sqm_npoints
 
