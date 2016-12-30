113a3e3d220a2e79d1e1c89d558d178b5823eb1e
snowman2 <alan.d.snow@usace.army.mil>
2016-12-30 08:53:10 -0600

    initial working version

---
+++ spt_ecmwf_autorapid_process/hpc/spt_hpc_watershed_groups_process.py
@@ -0,0 +1,112 @@
+import datetime
+import json
+import os
+import subprocess
+
+from ..imports.ftp_ecmwf_download import get_ftp_forecast_list,download_and_extract_ftp
+from ..imports.helper_functions import get_date_timestep_from_forecast_folder
+from ..rapid_process import update_lock_info_file
+
+
+# TODO: Count how many forecasts to run beforehand for each region and multiply expected runtime by that number
+# TODO: Wait to submit next forecast until previous job is finished, keep downloading if not ready to submit
+
+def spt_hpc_watershed_groups_process(main_log_directory,
+                                     ecmwf_forecast_location,
+                                     region_qsub_path,
+                                     region_reset_qsub_path,
+                                     ftp_host,
+                                     ftp_login,
+                                     ftp_passwd,
+                                     ftp_directory,
+                                     region_data_list,
+                                     hpc_project_number,
+                                     ):
+    '''
+    Process to run SPT on HPC
+
+    :param main_log_directory:
+    :param ecmwf_forecast_location:
+    :param region_qsub_path:
+    :param region_reset_qsub_path:
+    :param ftp_host:
+    :param ftp_login:
+    :param ftp_passwd:
+    :param ftp_directory:
+    :param region_data_list:
+    :param hpc_project_number:
+    :return:
+    '''
+
+    LOCK_INFO_FILE = os.path.join(main_log_directory, "ecmwf_rapid_run_info_lock.txt")
+
+    ecmwf_folders = sorted(get_ftp_forecast_list('Runoff.*.netcdf.tar*',
+                                                 ftp_host,
+                                                 ftp_login,
+                                                 ftp_passwd,
+                                                 ftp_directory))
+
+    # LOAD LOCK INFO FILE
+    last_forecast_date = datetime.datetime.utcfromtimestamp(0)
+    if os.path.exists(LOCK_INFO_FILE):
+        with open(LOCK_INFO_FILE) as fp_lock_info:
+            previous_lock_info = json.load(fp_lock_info)
+
+        if previous_lock_info['running']:
+            print("Another ECMWF-RAPID process is running.\n"
+                  "The lock file is located here: {0}\n"
+                  "If this is an error, you have two options:\n"
+                  "1) Delete the lock file.\n"
+                  "2) Edit the lock file and set \"running\" to false. \n"
+                  "Then, re-run this script. \n Exiting ...".format(LOCK_INFO_FILE))
+            return
+        else:
+            last_forecast_date = datetime.datetime.strptime(previous_lock_info['last_forecast_date'], '%Y%m%d%H')
+            run_ecmwf_folders = []
+            for ecmwf_folder in ecmwf_folders:
+                # get date
+                forecast_date_timestep = get_date_timestep_from_forecast_folder(ecmwf_folder)
+                forecast_date = datetime.datetime.strptime(forecast_date_timestep[:11], '%Y%m%d.%H')
+                # if more recent, add to list
+                if forecast_date > last_forecast_date:
+                    run_ecmwf_folders.append(ecmwf_folder)
+
+            ecmwf_folders = run_ecmwf_folders
+
+            if ecmwf_folders:
+                for ecmwf_folder in ecmwf_folders:
+                    # tell program that you are running/update to last downloaded file
+                    update_lock_info_file(LOCK_INFO_FILE, True, last_forecast_date.strftime('%Y%m%d%H'))
+                    # get datetime from folder
+                    last_forecast_date_timestep = get_date_timestep_from_forecast_folder(ecmwf_folder)
+                    last_forecast_date = datetime.datetime.strptime(last_forecast_date_timestep[:11], '%Y%m%d.%H')
+                    # download forecast
+                    try:
+                        download_and_extract_ftp(ecmwf_forecast_location, ecmwf_folder,
+                                                 ftp_host, ftp_login,
+                                                 ftp_passwd, ftp_directory,
+                                                 False)
+                        # SUBMIT JOBS IF DOWNLOAD
+                        for region_data in region_data_list:
+                            job_info = subprocess.check_output(['qsub',
+                                                                '-v', 'region_name={0}'.format(region_data['name']),
+                                                                '-o', 'spt_main_region_log_{0}.out'.format(region_data['name']),
+                                                                '-l', 'walltime={0}'.format(region_data['walltime']),
+                                                                '-A', hpc_project_number,
+                                                                region_qsub_path])
+                            # submit job after finish to release lock file
+                            job_number = job_info.split(".")[0]
+                            subprocess.call(['qsub',
+                                             '-v', 'region_name={0}'.format(region_data['name']),
+                                             '-o', 'spt_reset_region_log_{0}.out'.format(region_data['name']),
+                                             '-A', hpc_project_number,
+                                             '-W', 'depend=afterany:{0}'.format(job_number),
+                                             region_reset_qsub_path])
+                    except Exception:
+                        break
+                        pass
+                # release lock file
+                update_lock_info_file(LOCK_INFO_FILE, False, last_forecast_date.strftime('%Y%m%d%H'))
+
+            else:
+                print("No new forecasts found to run. Exiting ...")
