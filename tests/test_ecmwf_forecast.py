8d28fa3f548e46f6a4c94866d214e08d0f124962
snowman2 <alansnow21@gmail.com>
2017-08-11 15:03:09 -0500

    updated tests for seasonal initialization for ecmwf

--- tests/test_ecmwf_forecast.py
+++ tests/test_ecmwf_forecast.py
@@ -35,6 +35,55 @@ def test_ecmwf_forecast(ecmwf_setup):
     run_ecmwf_forecast_process(rapid_executable_location=RAPID_EXE_PATH,
                                rapid_io_files_location=ecmwf_setup.rapid_io_folder,
                                ecmwf_forecast_location=ecmwf_setup.lsm_folder,
+                               main_log_directory=ecmwf_setup.log_folder,
+                               subprocess_log_directory=ecmwf_setup.subprocess_log_folder,
+                               mp_execute_directory=ecmwf_setup.multiprocess_execute_folder,
+                               region=region,
+                               initialize_flows=True,
+                               download_ecmwf=False,
+                               mp_mode='multiprocess')
+
+    output_folder = os.path.join(ecmwf_setup.rapid_io_folder, 'output', watershed, out_forecast_folder)
+    # check log file exists
+    log_files = glob(os.path.join(ecmwf_setup.log_folder,
+                                  "spt_compute_ecmwf_{0:%y%m%d%H%M}*.log".format(start_datetime)))
+    assert len(log_files) == 1
+    # check Qout files
+    for qout_name in qout_names:
+        qout_file = os.path.join(output_folder, qout_name)
+        assert os.path.exists(qout_file)
+        compare_qout_file = os.path.join(ecmwf_setup.watershed_compare_folder,
+                                         out_forecast_folder,
+                                         qout_name)
+        with xr.open_dataset(qout_file) as xqf, \
+                xr.open_dataset(compare_qout_file) as xqc:
+            assert_almost_equal(xqf.Qout.values, xqc.Qout.values)
+            assert_array_equal(xqf.rivid.values, xqc.rivid.values)
+            assert_almost_equal(xqf.lat.values, xqc.lat.values)
+            assert_almost_equal(xqf.lon.values, xqc.lon.values)
+
+    # check Qinit file
+    assert os.path.exists(os.path.join(ecmwf_setup.watershed_input_folder, 'Qinit_20170708t00.csv'))
+
+
+def test_ecmwf_forecast_historical(ecmwf_setup):
+    """
+    Test basic ECMWF forecast process.
+    """
+    qout_names = [
+        'Qout_dominican_republic_haina_5.nc',
+        'Qout_dominican_republic_haina_50.nc',
+        'Qout_dominican_republic_haina_51.nc',
+        'Qout_dominican_republic_haina_52.nc',
+    ]
+    out_forecast_folder = '20170708.00'
+    watershed = 'dominican_republic-haina'
+    region = 'C.america'
+
+    start_datetime = datetime.utcnow()
+    run_ecmwf_forecast_process(rapid_executable_location=RAPID_EXE_PATH,
+                               rapid_io_files_location=ecmwf_setup.rapid_io_folder,
+                               ecmwf_forecast_location=ecmwf_setup.lsm_folder,
                                era_interim_data_location=ecmwf_setup.historical_input_folder,
                                main_log_directory=ecmwf_setup.log_folder,
                                subprocess_log_directory=ecmwf_setup.subprocess_log_folder,
@@ -55,7 +104,10 @@ def test_ecmwf_forecast(ecmwf_setup):
         qout_file = os.path.join(output_folder, qout_name)
         assert os.path.exists(qout_file)
 
-        compare_qout_file = os.path.join(ecmwf_setup.watershed_compare_folder, out_forecast_folder, qout_name)
+        compare_qout_name = os.path.splitext(qout_name)[0] + "_init.nc"
+        compare_qout_file = os.path.join(ecmwf_setup.watershed_compare_folder,
+                                         out_forecast_folder,
+                                         compare_qout_name)
         with xr.open_dataset(qout_file) as xqf, \
                 xr.open_dataset(compare_qout_file) as xqc:
             assert_almost_equal(xqf.Qout.values, xqc.Qout.values)
@@ -65,6 +117,7 @@ def test_ecmwf_forecast(ecmwf_setup):
 
     # check Qinit file
     assert os.path.exists(os.path.join(ecmwf_setup.watershed_input_folder, 'Qinit_20170708t00.csv'))
+
     # check warning points
     return_2_warnings = os.path.join(output_folder, "return_2_points.geojson")
     return_10_warnings = os.path.join(output_folder, "return_10_points.geojson")
@@ -85,4 +138,3 @@ def test_ecmwf_forecast(ecmwf_setup):
                                         out_forecast_folder,
                                         'return_20_points.geojson')
     compare_warnings(return_20_warnings, compare_return20_file)
-
