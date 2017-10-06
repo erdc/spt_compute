7106db19ab117c0716b5c137dcbe4e2309646f6e
msouff <masouffront@gmail.com>
2017-10-06 16:30:49 -0600

    fixed warning points bug

--- spt_compute/imports/generate_warning_points.py
+++ spt_compute/imports/generate_warning_points.py
@@ -192,7 +192,12 @@ def generate_ecmwf_warning_points(ecmwf_prediction_folder, return_period_file,
         std_ar = std_ds.isel(rivid=rivid_index)
         std_upper_ar = (mean_ar + std_ar)
         max_ar = max_ds.isel(rivid=rivid_index)
-        std_upper_ar[std_upper_ar > max_ar] = max_ar
+        
+	for i, val in enumerate(std_upper_ar > max_ar):
+            if val:
+                std_upper_ar[i] = max_ar[i]
+
+        #std_upper_ar[std_upper_ar > max_ar] = max_ar
 
         combinded_stats = pd.DataFrame({
             'mean': mean_ar.to_dataframe().Qout,
